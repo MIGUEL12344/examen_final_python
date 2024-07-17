b385be7b302c0935711ced94f4cc35a47d09b7ed
miguel12344 <ronalmiguel2005@gmail.com>
2024-07-17 09:29:32 -0500

    solucion: primer ejercicio

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,23 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+#crear una funcion
+def encontrar_menor_y_mayor(lista_numeros):
+    if len(lista_numeros) == 0:
+        return {} 
+    menor = lista_numeros[0]
+    mayor = lista_numeros[0]
+    for num in lista_numeros:
+        if num < menor:
+            menor = num
+        if num > mayor:
+            mayor = num
+    resultado = {
+        'menor': menor,
+        'mayor': mayor
+    }
+    return resultado
+# mostrar resultado
+resultado = encontrar_menor_y_mayor([4, 7, 10, 4, 1, 0])
+print(resultado)
+
