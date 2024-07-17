5bad8d036a5b7f2d3bf50c1a5830055dc617227d
miguel12344 <ronalmiguel2005@gmail.com>
2024-07-17 10:23:52 -0500

    solucion: quinto ejercicio

--- ejercicio_cinco.py
+++ ejercicio_cinco.py
@@ -4,3 +4,15 @@ ejm:
 entrada: ("jose","alvarez","30","APSTI","III")
 salida: {nombre:"jose",apellido:"alvarez",edad:"30",programa_estudio:"APSTI",semestre:"III"}
 """
+def datos_personales(nombre, apellido, edad, programa_estudio, semestre):
+#Función que retorna un diccionario con los datos personales de un alumno
+    return {
+        'nombre': nombre,
+        'apellido': apellido,
+        'edad': edad,
+        'programa_estudio': programa_estudio,
+        'semestre': semestre
+    }
+# mostrar por consola
+resultado = datos_personales("paco", "gerte", "25", "APSTI", "III")
+print(resultado) 
